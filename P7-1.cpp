198d1433d1c7d72e8ba8781fe1acb16fc6da23e2
swilt1onu <112091288+swilt1onu@users.noreply.github.com>
2022-10-06 10:02:38 -0400

    Add files via upload

---
+++ P7-1.cpp
@@ -0,0 +1,49 @@
+/*
+File: Lab5-4
+Selby Wilt
+program*/
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+int main() {
+	int locationX = 0;
+	int locationY = 0;
+	int time = 120 seconds;
+	int drunkard = 1
+	
+	const int MOVELEFT = locationX - 1 && locationY +0; 
+	const int MOVERIGHT = locationX + 1 && locationY +0;
+	const int MOVEUP = locationY + 1 && locationY +0;
+	const int MOVEDOWN = locationY - 1 && locationY +0;
+	const int MOVEUPRIGHT = locationX + 1 && locationY + 1;
+	const int MOVEUPLEFT = locationX - 1 && locationY + 1;
+	const int MOVEDOWNLEFT = locationX - 1 && locationY - 1;
+	const int MOVEDOWNRIGHT = locationX + 1 && locationY - 1;
+	const int NOMOVE = locationX + 0 and locationY + 0;
+
+	for (n = 1; n <= time; n++)
+	{
+		if      
+		if       
+		if    
+		if    
+		if
+		if
+		if
+		if
+		if
+		if 
+		
+	}
+
+	cout << "location of drunkard 1 is: (" << location x << "," << location y << ")";
+		
+
+
+
+	
+}
